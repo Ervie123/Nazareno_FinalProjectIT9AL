657d9da6da3a840a99a9a1ce39227f77b8bd37fa
e.nazareno <e.nazareno.553857@umindanao.edu.ph>
2026-05-21 10:27:49 +0800

    Initial commit

--- storage/framework/views/62190de0d4eb43d65aa52d914f887899.php
+++ storage/framework/views/62190de0d4eb43d65aa52d914f887899.php
@@ -9,6 +9,8 @@
   <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600;700&family=DM+Serif+Display&display=swap" rel="stylesheet"/>
 
  
+
+ <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
  <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
 
 </head>
