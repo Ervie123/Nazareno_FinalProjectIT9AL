8af9035c26ef02e50e90e02d116d9618d4952623
e.nazareno <e.nazareno.553857@umindanao.edu.ph>
2026-05-22 18:17:25 +0800

    Update render config

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com"/>
   <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;600;700&family=DM+Serif+Display&display=swap" rel="stylesheet"/>
 
-<link rel="stylesheet" href="{{ asset('build/assets/app-DwgYjyMZ.css') }}">
-<script type="module" src="{{ asset('build/assets/app-BFiU_cls.js') }}"></script>
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   <style>
